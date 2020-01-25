69d651e11e74ab90e7e66caa84fd200ccc48a0fb
qbhatti <qasimbhatti@hotmail.com>
2020-01-25 12:27:30 -0800

    Create custom useLocalStorageReducer hook to use local storage and useReducer

--- src/contexts/todos.context.js
+++ src/contexts/todos.context.js
@@ -1,5 +1,6 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext } from "react";
 import todoReducer from "../reducers/todo.reducer";
+import useLocalStorageReducer from "../hooks/useLocalStorageReducer";
 
 const defaultTodos = [
   { id: 1, task: "Mow the lawn using goats", completed: false },
@@ -10,7 +11,11 @@ export const TodosContext = createContext();
 export const DispatchContext = createContext();
 
 export function TodosProvider({ children }) {
-  const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
+  const [todos, dispatch] = useLocalStorageReducer(
+    "todos",
+    defaultTodos,
+    todoReducer
+  );
 
   return (
     <TodosContext.Provider value={todos}>
